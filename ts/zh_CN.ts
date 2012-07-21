531fc4e47a757dd34d389b8aeaba7edcf002ba61
shanyan baishui <Siramizu@gmail.com>
2012-07-21 11:50:03 +0200

    Translated using Weblate.

--- ts/zh_CN.ts
+++ ts/zh_CN.ts
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0">
 <defaultcodec>UTF-8</defaultcodec>
@@ -7,37 +6,37 @@
     <message>
         <location filename="../qml/gepes/AboutDialog.qml" line="27"/>
         <source>GPS and compass based tools</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">基于 GPS 和指南针的工具</translation>
     </message>
     <message>
         <location filename="../qml/gepes/AboutDialog.qml" line="31"/>
         <source>Version:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">版本:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/AboutDialog.qml" line="39"/>
         <source>Email:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">邮箱:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/AboutDialog.qml" line="43"/>
         <source>Website:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">网站:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/AboutDialog.qml" line="52"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">确定</translation>
     </message>
     <message>
         <location filename="../qml/gepes/AboutDialog.qml" line="56"/>
         <source>Open website</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">访问网站</translation>
     </message>
     <message>
         <location filename="../qml/gepes/AboutDialog.qml" line="60"/>
         <source>Donate</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">捐助</translation>
     </message>
 </context>
 <context>
@@ -45,12 +44,12 @@
     <message>
         <location filename="../qml/gepes/CompassPage.qml" line="29"/>
         <source>Azimuth:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">方位:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/CompassPage.qml" line="29"/>
         <source>accuracy</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">精度</translation>
     </message>
 </context>
 <context>
@@ -58,12 +57,12 @@
     <message>
         <location filename="../qml/gepes/GPSBase.qml" line="10"/>
         <source>Position</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">位置</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSBase.qml" line="16"/>
         <source>Latitude:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">纬度:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSBase.qml" line="16"/>
@@ -71,22 +70,22 @@
         <location filename="../qml/gepes/GPSBase.qml" line="24"/>
         <location filename="../qml/gepes/GPSBase.qml" line="28"/>
         <source>N/A</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">不可用</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSBase.qml" line="20"/>
         <source>Longitude:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">经度:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSBase.qml" line="24"/>
         <source>Altitude:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">高度:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSBase.qml" line="28"/>
         <source>Speed:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">速度:</translation>
     </message>
 </context>
 <context>
@@ -94,28 +93,28 @@
     <message>
         <location filename="../qml/gepes/GPSPage.qml" line="26"/>
         <source>Accuracy</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">精度</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSPage.qml" line="32"/>
         <source>Vertical:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">垂直:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSPage.qml" line="32"/>
         <location filename="../qml/gepes/GPSPage.qml" line="36"/>
         <source>N/A</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">不可用</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSPage.qml" line="36"/>
         <source>Horizontal:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">水平:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/GPSPage.qml" line="40"/>
         <source>Source:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">信号源:</translation>
     </message>
 </context>
 <context>
@@ -123,27 +122,27 @@
     <message>
         <location filename="../qml/gepes/MainPage.qml" line="23"/>
         <source>Tools</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">工具</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MainPage.qml" line="29"/>
         <source>Sun</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">太阳</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MainPage.qml" line="34"/>
         <source>Moon</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">月亮</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MainPage.qml" line="39"/>
         <source>Compass</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">指南针</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MainPage.qml" line="44"/>
         <source>GPS details</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GPS 详细信息</translation>
     </message>
 </context>
 <context>
@@ -151,72 +150,72 @@
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="48"/>
         <source>Moon</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">月亮</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="59"/>
         <source>Moon phase</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">月相</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="77"/>
         <source>New moon</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">新月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="79"/>
         <source>Waxing crescent</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">娥眉月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="81"/>
         <source>First quarter</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">上弦月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="83"/>
         <source>Waxing gibbous</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">盈凸月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="85"/>
         <source>Full moon</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">满月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="87"/>
         <source>Waning gibbous</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">亏凸月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="89"/>
         <source>Last quarter</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">下弦月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="91"/>
         <source>Waning crescent</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">残月</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="93"/>
         <source>Dark moon</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">晦</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="116"/>
         <source>Choose date</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">选择日期</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="117"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">确定</translation>
     </message>
     <message>
         <location filename="../qml/gepes/MoonPage.qml" line="118"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">取消</translation>
     </message>
 </context>
 <context>
@@ -224,22 +223,22 @@
     <message>
         <location filename="../qml/gepes/SettingsPage.qml" line="22"/>
         <source>Sensors</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">传感器</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SettingsPage.qml" line="28"/>
         <source>Refresh frequency:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">刷新频率:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SettingsPage.qml" line="28"/>
         <source>auto</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">自动</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SettingsPage.qml" line="49"/>
         <source>Appearance</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">外观</translation>
     </message>
 </context>
 <context>
@@ -247,17 +246,17 @@
     <message>
         <location filename="../qml/gepes/SunCompassPage.qml" line="41"/>
         <source>Sun azimuth:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">太阳方位:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunCompassPage.qml" line="49"/>
         <source>Azimuth:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">方位:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunCompassPage.qml" line="49"/>
         <source>accuracy</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">精度</translation>
     </message>
 </context>
 <context>
@@ -265,49 +264,49 @@
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="48"/>
         <source>Sun</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">太阳</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="58"/>
         <location filename="../qml/gepes/SunPage.qml" line="105"/>
         <source>Sun rise:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">日出:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="69"/>
         <source>Solar noon:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">日正:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="80"/>
         <location filename="../qml/gepes/SunPage.qml" line="119"/>
         <source>Sun set:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">日落:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="95"/>
         <source>Azimuth</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">方位</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="132"/>
         <source>Sun position:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">太阳位置:</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="169"/>
         <source>Choose date</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">选择日期</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="170"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">确定</translation>
     </message>
     <message>
         <location filename="../qml/gepes/SunPage.qml" line="171"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">取消</translation>
     </message>
 </context>
 </TS>
